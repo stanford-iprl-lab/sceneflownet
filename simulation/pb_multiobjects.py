5134bb057a885668ec78f9d99a0d01663af91ec8
linsats <lins2@stanford.edu>
2017-12-09 17:04:51 -0800

    update pb_multiobjects.py

--- simulation/pb_multiobjects.py
+++ simulation/pb_multiobjects.py
@@ -30,16 +30,26 @@ class pybullet_objects:
       pos = [0,0,i*1.0+0.3]
       orn = self.p.getQuaternionFromEuler(self.EulerList[i])
       self.p.createMultiBody(baseMass = mass, baseCollisionShapeIndex = modelId, baseVisualShapeIndex = visualShapeId, basePosition = pos, baseOrientation = orn) 
-      self.p.changeDynamics(modelId,-1,spinningFriction = 0.001, rollingFriction = 0.001, linearDamping = 0.0)
+      self.p.changeDynamics(modelId,-1,spinningFriction = 0.001, rollingFriction = 0.001, linearDamping = 0.3)
 
     self.p.setGravity(0,0,-10)
+
+    self.endEffectorId = self.p.createCollisionShape(p.GEOM_BOX,halfExtents=[0.05,0.05,0.3])
+    self.endEffectorPos = [0,0,0.3]
+    self.endEffectorOrn = self.p.getQuaternionFromEuler([0.5,0.5,0])
+    self.p.createMultiBody(baseMass = 0, baseCollisionShapeIndex = self.endEffectorId, basePosition = self.endEffectorPos, baseOrientation = self.endEffectorOrn)    
    
   def simulation(self):
     self.p.setRealTimeSimulation(1)
+    frame_id = 0.0
     while(1):
       self.p.getKeyboardEvents()
-      p.getCameraImage(320,320)
+      image = p.getCameraImage(320,320)
+      self.endEffectorOrn = self.p.getQuaternionFromEuler([0.5,0.5,0.1*frame_id])
+      self.p.resetBasePositionAndOrientation(self.endEffectorId,self.endEffectorPos,self.endEffectorOrn)
       time.sleep(0.01) 
+      frame_id += 1.0
 
-test = pybullet_objects(['./model1.obj','./model2.obj','./model3.obj'],gui=True)
-test.simulation()
+if __name__ == "__main__":    
+  test = pybullet_objects(['./model1.obj','./model2.obj','./model3.obj'],gui=True)
+  test.simulation()
