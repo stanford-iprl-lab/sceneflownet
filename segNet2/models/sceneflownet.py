bc10d390a73913d2923c166d5baa6ba2a0adf7d0
linsats <lins2@stanford.edu>
2018-01-10 06:12:31 +0000

    update sceneflownet.py

--- segNet2/models/sceneflownet.py
+++ segNet2/models/sceneflownet.py
@@ -2,12 +2,28 @@ import numpy as np
 import tensorflow as tf
 import tflearn
 import sys
-sys.path.append('/home/lins/interactive-segmentation/segNet2/src')
+sys.path.append('/home/linshaonju/interactive-segmentation/segNet2/src')
 from correlation import correlation
 from utils import LeakyReLU
 
+def encoder_rgb(x,reuse=False):
+  with tf.name_scope("model_rgb"):
+    x = tflearn.layers.conv.conv_2d(x,16,(3,3),strides=1,activation='relu',weight_decay=1e-5,regularizer='L2',reuse=reuse,scope="conv1_1_rgb")
+    x = tflearn.layers.conv.conv_2d(x,16,(3,3),strides=1,activation='relu',weight_decay=1e-5,regularizer='L2',reuse=reuse,scope="conv1_2_rgb")
+    x = tflearn.layers.conv.conv_2d(x,16,(3,3),strides=2,activation='relu',weight_decay=1e-5,regularizer='L2',reuse=reuse,scope="conv1_3_rgb")
+##120,160
+    x = tflearn.layers.conv.conv_2d(x,16,(3,3),strides=1,activation='relu',weight_decay=1e-5,regularizer='L2',reuse=reuse,scope="conv2_1_rgb")
+    x = tflearn.layers.conv.conv_2d(x,16,(3,3),strides=1,activation='relu',weight_decay=1e-5,regularizer='L2',reuse=reuse,scope="conv2_2_rgb")
+    x = tflearn.layers.conv.conv_2d(x,32,(3,3),strides=2,activation='relu',weight_decay=1e-5,regularizer='L2',reuse=reuse,scope="conv2_3_rgb")
+##60,80
+    x = tflearn.layers.conv.conv_2d(x,32,(3,3),strides=1,activation='relu',weight_decay=1e-5,regularizer='L2',reuse=reuse,scope="conv3_1_rgb")
+    x = tflearn.layers.conv.conv_2d(x,32,(3,3),strides=1,activation='relu',weight_decay=1e-5,regularizer='L2',reuse=reuse,scope="conv3_2_rgb")
+    x = tflearn.layers.conv.conv_2d(x,64,(3,3),strides=2,activation='relu',weight_decay=1e-5,regularizer='L2',reuse=reuse,scope="conv3_3_rgb")
+##30,40
+  return x
+
 def encoder(x,reuse=False):
-  with tf.name_scope("model"):
+  with tf.name_scope("model_xyz"):
     x = tflearn.layers.conv.conv_2d(x,16,(3,3),strides=1,activation='relu',weight_decay=1e-5,regularizer='L2',reuse=reuse,scope="conv1_1")
     x = tflearn.layers.conv.conv_2d(x,16,(3,3),strides=1,activation='relu',weight_decay=1e-5,regularizer='L2',reuse=reuse,scope="conv1_2")
     x = tflearn.layers.conv.conv_2d(x,16,(3,3),strides=2,activation='relu',weight_decay=1e-5,regularizer='L2',reuse=reuse,scope="conv1_3")
@@ -23,52 +39,42 @@ def encoder(x,reuse=False):
   return x
 
 
-def generate_cc_weight_matrix(h=30,w=40,r=10):
-  w_cc_mat = h*w*(2*r+1)*(2*r+1)
-  h_cc_mat = h * w
-  cc_mat = np.zeros((w_cc_mat, h_cc_mat))
-  for i in xrange(30):
-    for j in xrange(40):
-      for i_shift in xrange(-r,r+1):
-        for j_shift in xrange(-r,r+1):
-          index_w = j + j_shift
-          index_h = i + i_shift
-          #if index_w >= 0 and index_w < w_cc_mat
-
-
 rad = 10
+dia = 2 * rad + 1
 
 def cnnmodel(frame1_xyz,frame1_rgb,frame2_xyz,frame2_rgb):
-  frame1_input = tf.concat([frame1_xyz,frame1_rgb],3)
-  frame2_input = tf.concat([frame2_xyz,frame2_rgb],3)
+  frame1_feat_rgb = encoder_rgb(frame1_rgb)
+  frame2_feat_rgb = encoder_rgb(frame2_rgb,reuse=True)
 
-  frame1_feat = encoder(frame1_input)
-  frame2_x = encoder(frame2_input,reuse=True)
+  frame1_feat = encoder(frame1_xyz)
+  frame2_feat = encoder(frame2_xyz,reuse=True)
+  
+  frame2_feat_rgb = tf.pad(frame2_feat_rgb,paddings=[[0,0],[1,1],[0,0],[0,0]])
+  frame1_feat_rgb = tf.pad(frame1_feat_rgb,paddings=[[0,0],[1,1],[0,0],[0,0]])
 
-  cc = correlation(frame2_x,frame1_feat,1,rad,1,1,rad)
- 
-  cc_relu = LeakyReLU(cc)
- 
-  frame1_feat = tf.transpose(frame1_feat,[0,3,1,2])
- 
+  cc = correlation(frame2_feat_rgb,frame1_feat_rgb,1,rad,1,1,rad)[:,1:-1,:,:]
+  cc_relu = tf.nn.relu(cc)
+
+  frame1_feat = tf.transpose(frame1_feat,[0,3,1,2]) 
   frame1_feat_padded = tf.pad(frame1_feat,paddings=[[0,0],[0,0],[rad,rad],[rad,rad]])
   
   frame1_list = []
+
   for i in xrange(30):
     for j in xrange(40):
       tmp = frame1_feat_padded[:,:,0+i:2*rad+1+i,0+j:2*rad+1+j]
-      tmp = tf.reshape(tmp,[-1,64,11*11])
+      tmp = tf.reshape(tmp,[-1,64,dia * dia])
       frame1_list.append(tmp)
-
+ 
   frame1_list = tf.stack(frame1_list,axis=2)
   frame1_list = tf.transpose(frame1_list,[0,2,3,1])
-  cc_relu = tf.reshape(cc_relu,[-1,30*40,121,1])
-
+  cc_relu = tf.reshape(cc_relu,[-1,30*40,dia * dia,1])
+  
   frame1_list = frame1_list * cc_relu
-  frame1_list = tf.nn.max_pool(frame1_list,ksize=[1,1,121,1],strides=[1,1,121,1],padding='VALID')
+  frame1_list = tf.nn.max_pool(frame1_list,ksize=[1,1,dia * dia,1],strides=[1,1,dia * dia,1],padding='VALID')
   frame1_list = tf.reshape(frame1_list,(-1,30,40,64))
   
-  x = tf.concat([frame2_x,frame1_list],3)
+  x = tf.concat([frame2_feat,frame1_list],3)
 
   x=tflearn.layers.conv.conv_2d(x,64,(3,3),strides=1,activation='relu',weight_decay=1e-5,regularizer='L2')
   x=tflearn.layers.conv.conv_2d(x,64,(3,3),strides=1,activation='relu',weight_decay=1e-5,regularizer='L2')
