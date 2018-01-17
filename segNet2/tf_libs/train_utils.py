432252816fc757c332c2d1e90c4308a6e05567f8
linsats <lins2@stanford.edu>
2018-01-17 01:36:24 +0000

    update

--- segNet2/tf_libs/train_utils.py
+++ segNet2/tf_libs/train_utils.py
@@ -10,7 +10,39 @@ import tensorflow as tf
 slim = tf.contrib.slim
 FLAGS_checkpoint_exclude_scopes=None
 FLAGS_checkpoint_include_scopes='resnet_v1_50'
+def get_var_list_to_restore_by_name(var_name=None):
+  """Choose which vars to restore, ignore vars by setting --checkpoint_exclude_scopes """
 
+  variables_to_restore = []
+  if FLAGS_checkpoint_exclude_scopes is not None:
+    exclusions = [scope.strip()
+                  for scope in FLAGS_checkpoint_exclude_scopes.split(',')]
+
+    # build restore list
+    for var in tf.model_variables():
+      for exclusion in exclusions:
+        if var.name.startswith(exclusion):
+          break
+      else:
+        variables_to_restore.append(var)
+  else:
+    variables_to_restore = tf.model_variables()
+
+  variables_to_restore_final = []
+  if var_name is not None:
+      includes = [
+              scope.strip()
+              for scope in var_name.split(',')
+              ]
+      for var in variables_to_restore:
+          for include in includes:
+              if var.name.startswith(include):
+                  variables_to_restore_final.append(var)
+                  break
+  else:
+      variables_to_restore_final = variables_to_restore
+
+  return variables_to_restore_final
 def get_var_list_to_restore():
   """Choose which vars to restore, ignore vars by setting --checkpoint_exclude_scopes """
 
