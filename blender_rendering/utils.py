616bcf23937106664e13620378c9c6df4f7f8555
linsats <lins2@stanford.edu>
2018-01-24 05:38:13 +0000

    update blender

---
+++ blender_rendering/utils.py
@@ -0,0 +1,180 @@
+import bpy
+import math 
+import sys
+import os
+import numpy as np
+from numpy.linalg import inv
+from math import *
+import mathutils
+from mathutils import Matrix
+
+def makeMaterial(name, diffuse, specular, alpha):
+    mat = bpy.data.materials.new(name)
+    mat.diffuse_color = diffuse
+    mat.diffuse_shader = 'LAMBERT'
+    mat.diffuse_intensity = 1.0
+    mat.specular_color = specular
+    mat.specular_shader = 'COOKTORR'
+    mat.specular_intensity = 0.5
+    mat.alpha = alpha
+    mat.ambient = 1
+    return mat
+
+def setMaterial(ob, mat):
+    me = ob.data
+    me.materials.append(mat)
+
+def obj_centened_camera_pos(dist, azimuth_deg, elevation_deg):
+    '''
+       Args:
+           dist :         distance from camera position to the origin
+           azimuth_deg:   azimuth degree in 360 degresses starting from positive x axis
+           elevation_deg: elevation_deg in 360 degresses
+       Returns:
+           (x,y,z) :      coordinates in xyz 
+    '''
+    phi = float(elevation_deg) / 180 * math.pi
+    theta = float(azimuth_deg) / 180 * math.pi
+    x = (dist * math.cos(theta) * math.cos(phi))
+    y = (dist * math.sin(theta) * math.cos(phi))
+    z = (dist * math.sin(phi))
+    return (x, y, z)
+
+def quaternionFromYawPitchRoll(yaw, pitch, roll):
+    '''
+       Calculate quaternion from yaw, pitch, roll
+       Args:
+            yaw: rotation degress along z axis
+            pitch: rotation degrees along y axis
+            roll: rotation degress along x axis
+       Return:
+            quaternion
+    '''
+    c1 = math.cos(yaw / 2.0)
+    c2 = math.cos(pitch / 2.0)
+    c3 = math.cos(roll / 2.0)
+    s1 = math.sin(yaw / 2.0)
+    s2 = math.sin(pitch / 2.0)
+    s3 = math.sin(roll / 2.0)
+    q1 = c1 * c2 * c3 + s1 * s2 * s3
+    q2 = c1 * c2 * s3 - s1 * s2 * c3
+    q3 = c1 * s2 * c3 + s1 * c2 * s3
+    q4 = s1 * c2 * c3 - c1 * s2 * s3
+    return (q1, q2, q3, q4)
+
+def camPosToQuaternion(cx, cy, cz):
+    q1a = 0
+    q1b = 0
+    q1c = math.sqrt(2) / 2
+    q1d = math.sqrt(2) / 2
+    camDist = math.sqrt(cx * cx + cy * cy + cz * cz)
+    cx = cx / camDist
+    cy = cy / camDist
+    cz = cz / camDist
+    t = math.sqrt(cx * cx + cy * cy)
+    tx = cx / t
+    ty = cy / t
+    yaw = math.acos(ty)
+    if tx > 0:
+        yaw = 2 * math.pi - yaw
+    pitch = 0
+    tmp = min(max(tx*cx + ty*cy, -1),1)
+    roll = math.acos(tmp)
+    if cz < 0:
+        roll = -roll
+    q2a, q2b, q2c, q2d = quaternionFromYawPitchRoll(yaw, pitch, roll)
+    q1 = q1a * q2a - q1b * q2b - q1c * q2c - q1d * q2d
+    q2 = q1b * q2a + q1a * q2b + q1d * q2c - q1c * q2d
+    q3 = q1c * q2a - q1d * q2b + q1a * q2c + q1b * q2d
+    q4 = q1d * q2a + q1c * q2b - q1b * q2c + q1a * q2d
+    return (q1, q2, q3, q4)
+
+
+def camRotQuaternion(cx, cy, cz, theta):
+    theta = theta / 180.0 * math.pi
+    camDist = math.sqrt(cx * cx + cy * cy + cz * cz)
+    cx = -cx / camDist
+    cy = -cy / camDist
+    cz = -cz / camDist
+    q1 = math.cos(theta * 0.5)
+    q2 = -cx * math.sin(theta * 0.5)
+    q3 = -cy * math.sin(theta * 0.5)
+    q4 = -cz * math.sin(theta * 0.5)
+    return (q1, q2, q3, q4)
+
+def quaternionProduct(qx, qy):
+    '''
+       qx * qy
+       first rotation qy and then rotate qx
+    '''
+    a = qx[0]
+    b = qx[1]
+    c = qx[2]
+    d = qx[3]
+    e = qy[0]
+    f = qy[1]
+    g = qy[2]
+    h = qy[3]
+    q1 = a * e - b * f - c * g - d * h
+    q2 = a * f + b * e + c * h - d * g
+    q3 = a * g - b * h + c * e + d * f
+    q4 = a * h + b * g - c * f + d * e
+    return (q1, q2, q3, q4)
+
+def get_calibration_matrix_K_from_blender(camd):
+    f_in_mm = camd.lens
+    print('camaera focal length')
+    print(f_in_mm)
+    scene = bpy.context.scene
+    resolution_x_in_px = scene.render.resolution_x
+    resolution_y_in_px = scene.render.resolution_y
+    scale = scene.render.resolution_percentage / 100
+    sensor_width_in_mm = camd.sensor_width
+    sensor_height_in_mm = camd.sensor_height
+    pixel_aspect_ratio = scene.render.pixel_aspect_x / scene.render.pixel_aspect_y 
+    print("resolution_x_in_px")
+    print(resolution_x_in_px)
+    print("resolution_y_in_px")
+    print(resolution_y_in_px)
+    print("sensor_width_in_mm")
+    print(sensor_width_in_mm)
+    print("sensor_height_in_mm")
+    print(sensor_height_in_mm)
+    print("scene render pixel aspect ratio")
+    print(pixel_aspect_ratio)
+    print("scale")
+    print(scale)
+    if (camd.sensor_fit == 'VERTICAL'):
+        print("vertical")
+        # the sensor height is fixed (sensor fit is horizontal), 
+        # the sensor width is effectively changed with the pixel aspect ratio
+        s_u = resolution_x_in_px * scale / sensor_width_in_mm / pixel_aspect_ratio 
+        s_v = resolution_y_in_px * scale / sensor_height_in_mm
+    else: # 'HORIZONTAL' and 'AUTO'
+        print('AUTO')
+        # the sensor width is fixed (sensor fit is horizontal), 
+        # the sensor height is effectively changed with the pixel aspect ratio
+        pixel_aspect_ratio = scene.render.pixel_aspect_x / scene.render.pixel_aspect_y
+        s_u = resolution_x_in_px * scale / sensor_width_in_mm
+        s_v = resolution_y_in_px * scale * pixel_aspect_ratio / sensor_height_in_mm     
+    print("s_u")
+    print(s_u)
+    print("s_v")
+    print(s_v)
+    # Parameters of intrinsic calibration matrix K
+    alpha_u = f_in_mm * s_u
+    alpha_v = f_in_mm * s_v
+    u_0 = resolution_x_in_px*scale / 2
+    v_0 = resolution_y_in_px*scale / 2
+    skew = 0 # only use rectangular pixels
+
+    K = Matrix(
+        ((alpha_u, skew,    u_0),
+        (    0  ,  alpha_v, v_0),
+        (    0  ,    0,      1 )))
+    return K
+
+if __name__ == "__main__":
+    # Insert your camera name below
+    K = get_calibration_matrix_K_from_blender(bpy.data.objects['Camera'].data)
+    print(K)
