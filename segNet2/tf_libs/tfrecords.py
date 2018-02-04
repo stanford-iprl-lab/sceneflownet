55afed72bce12df6d07090d7543fde109410f4d0
linsats <lins2@stanford.edu>
2018-02-04 04:22:31 +0000

    add tfrecords

--- segNet2/tf_libs/tfrecords.py
+++ segNet2/tf_libs/tfrecords.py
@@ -23,13 +23,10 @@ def read_and_decode(filename_queue):
       'in_2frame_xyz':tf.FixedLenFeature([],tf.string),
       'in_1frame_rgb':tf.FixedLenFeature([],tf.string),
       'in_2frame_rgb':tf.FixedLenFeature([],tf.string), 
-      'outs_1frame_xyz':tf.FixedLenFeature([],tf.string),
       'outs_2frame_xyz':tf.FixedLenFeature([],tf.string),
       'outs_1frame_id':tf.FixedLenFeature([],tf.string),
       'outs_2frame_id':tf.FixedLenFeature([],tf.string), 
-      'outs_1frame_r': tf.FixedLenFeature([],tf.string), 
       'outs_2frame_r': tf.FixedLenFeature([],tf.string),
-      'outs_1frame_score':tf.FixedLenFeature([],tf.string), 
       'outs_2frame_score':tf.FixedLenFeature([],tf.string),  
       'outs_trans_translation':tf.FixedLenFeature([],tf.string),
       'outs_trans_rot':tf.FixedLenFeature([],tf.string),
@@ -43,16 +40,13 @@ def read_and_decode(filename_queue):
   in_1frame_rgb= tf.decode_raw(features['in_1frame_rgb'],tf.float32)
   in_2frame_rgb= tf.decode_raw(features['in_2frame_rgb'],tf.float32)
  
-  outs_1frame_xyz = tf.decode_raw(features['outs_1frame_xyz'],tf.float32)
   outs_2frame_xyz = tf.decode_raw(features['outs_2frame_xyz'],tf.float32)
   outs_1frame_id = tf.decode_raw(features['outs_1frame_id'],tf.float32)
   outs_2frame_id = tf.decode_raw(features['outs_2frame_id'],tf.float32)
 
-  outs_1frame_score = tf.decode_raw(features['outs_1frame_score'],tf.float32)
   outs_2frame_score = tf.decode_raw(features['outs_2frame_score'],tf.float32)
 
   outs_2frame_r = tf.decode_raw(features['outs_2frame_r'],tf.float32)
-  outs_1frame_r = tf.decode_raw(features['outs_1frame_r'],tf.float32)
 
   outs_trans_translation = tf.decode_raw(features['outs_trans_translation'],tf.float32)
   outs_trans_rot = tf.decode_raw(features['outs_trans_rot'],tf.float32)
@@ -65,15 +59,12 @@ def read_and_decode(filename_queue):
   in_1frame_rgb = tf.reshape(in_1frame_rgb,[h,w,3])
   in_2frame_rgb = tf.reshape(in_2frame_rgb,[h,w,3])
  
-  outs_1frame_xyz = tf.reshape(outs_1frame_xyz,[h,w,3]) 
   outs_2frame_xyz = tf.reshape(outs_2frame_xyz,[h,w,3])
   outs_1frame_id = tf.reshape(outs_1frame_id,[h,w]) 
   outs_2frame_id = tf.reshape(outs_2frame_id,[h,w]) 
 
   outs_2frame_r = tf.reshape(outs_2frame_r,[h,w,1])
-  outs_1frame_r = tf.reshape(outs_1frame_r,[h,w,1])
 
-  outs_1frame_score = tf.reshape(outs_1frame_score,[h,w])
   outs_2frame_score = tf.reshape(outs_2frame_score,[h,w])
  
   outs_trans_translation = tf.reshape(outs_trans_translation,[h,w,3]) 
@@ -90,11 +81,8 @@ def read_and_decode(filename_queue):
          in_2frame_rgb, \
          outs_1frame_id,\
          outs_2frame_id,\
-         outs_1frame_xyz, \
          outs_2frame_xyz, \
-         outs_1frame_r, \
          outs_2frame_r,\
-         outs_1frame_score, \
          outs_2frame_score, \
          outs_trans_translation, \
          outs_trans_rot,\
@@ -106,7 +94,7 @@ def read_and_decode(filename_queue):
 def inputs(batch_size,num_epochs,tfrecords_filename):
   with tf.name_scope('inputf'):
     filename_queue = tf.train.string_input_producer(
-      [tfrecords_filename], num_epochs=num_epochs)
+      tfrecords_filename, num_epochs=num_epochs)
 
     in_1frame_xyz_, \
       in_1frame_rgb_, \
@@ -114,11 +102,8 @@ def inputs(batch_size,num_epochs,tfrecords_filename):
       in_2frame_rgb_, \
       gt_1frame_id_, \
       gt_2frame_id_, \
-      gt_1frame_xyz_, \
       gt_2frame_xyz_, \
-      gt_1frame_r_, \
       gt_2frame_r_, \
-      gt_1frame_score_, \
       gt_2frame_score_, \
       gt_translation_, \
       gt_rot_, \
@@ -132,11 +117,8 @@ def inputs(batch_size,num_epochs,tfrecords_filename):
       in_2frame_rgb,\
       gt_1frame_id, \
       gt_2frame_id,\
-      gt_1frame_xyz,\
       gt_2frame_xyz, \
-      gt_1frame_r, \
       gt_2frame_r, \
-      gt_1frame_score, \
       gt_2frame_score, \
       gt_translation, \
       gt_rot, \
@@ -149,11 +131,8 @@ def inputs(batch_size,num_epochs,tfrecords_filename):
          in_2frame_rgb_,\
          gt_1frame_id_,\
          gt_2frame_id_,\
-         gt_1frame_xyz_, \
          gt_2frame_xyz_, \
-         gt_1frame_r_, \
          gt_2frame_r_, \
-         gt_1frame_score_ , \
          gt_2frame_score_, \
          gt_translation_, \
          gt_rot_,\
@@ -170,11 +149,8 @@ def inputs(batch_size,num_epochs,tfrecords_filename):
            in_2frame_rgb,\
            gt_1frame_id, \
            gt_2frame_id,\
-           gt_1frame_xyz,\
            gt_2frame_xyz,\
-           gt_1frame_r, \
            gt_2frame_r, \
-           gt_1frame_score, \
            gt_2frame_score,\
            gt_translation,\
            gt_rot,\
