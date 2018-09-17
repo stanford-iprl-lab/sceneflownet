c39731747cce14d52589ad86756d22259ce49371
linsats <lins2@stanford.edu>
2018-09-17 06:49:55 +0000

    update

--- segNet2/settingup_caffe.py
+++
@@ -1,8 +0,0 @@
-sudo apt-get install --no-install-recommends libboost-all-dev
-sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-serial-dev libgflags-dev libgoogle-glog-dev liblmdb-dev protobuf-compiler
-
-
-include this...
-
-INCLUDE_DIRS := $(PYTHON_INCLUDE) /usr/local/include /usr/include/hdf5/serial
-LIBRARY_DIRS := $(PYTHON_LIB) /usr/local/lib /usr/lib /usr/lib/aarch64-linux-gnu/hdf5/serial
