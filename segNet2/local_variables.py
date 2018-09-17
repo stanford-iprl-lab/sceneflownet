d15af58477339dd9fc077f29a9ac4cc7d55b3a54
linsats <lins2@stanford.edu>
2018-09-17 18:19:36 +0000

    update local_variables.py

--- segNet2/local_variables.py
+++ segNet2/local_variables.py
@@ -3,7 +3,5 @@ import sys
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.abspath(os.path.join(BASE_DIR,os.pardir,'Data'))
-GRASP_DIR = os.path.abspath(os.path.join(BASE_DIR,os.pardir,'grasping'))
 ROOT_DIR = os.path.abspath(os.path.join(BASE_DIR, os.pardir))
 
-pygeometry_dir = '/home/lins/pygeometry'
