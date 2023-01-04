22700deb82cdc355524ef5f593a96f76785fc649
removedporn <86824510+removedporn@users.noreply.github.com>
2023-01-04 12:25:19 +0800

    Delete blender_rendering directory

--- blender_rendering/local_variables.py
+++
@@ -1,7 +0,0 @@
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT_DIR = os.path.join(BASE_DIR, os.pardir)
-DATA_DIR = os.path.join(ROOT_DIR,'Data')
-mesh_top_dir = os.path.join(DATA_DIR,'GraspNet')
-scan_result_dir = os.path.join(DATA_DIR,'BlensorResult')
-CommandFile_dir = os.path.join(DATA_DIR,'CommandFiles')
